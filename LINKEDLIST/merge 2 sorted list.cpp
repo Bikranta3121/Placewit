280aa723e7000bbc655dec8746ef1b068f51433c
BIKRANTA KUMAR BEHERA <106620478+Bikranta3121@users.noreply.github.com>
2023-03-26 10:34:01 +0530

    Create merge 2 sorted list.cpp

---
+++ LINKEDLIST/merge 2 sorted list.cpp
@@ -0,0 +1,37 @@
+Node* sortedMerge(Node* list1, Node* list2)  
+{  
+   Node *l1=NULL;
+    Node *l2=NULL;
+    Node *n1=NULL;
+    Node *res=NULL;
+    if(!list1) return list2;
+    if(!list2) return list1;
+    if(list1->data <= list2->data)
+    {
+        l1=list1;
+        l2=list2;
+    }
+    else
+    {
+        l1=list2;
+        l2=list1;
+    }
+    res=l1;
+    n1=l1;
+    while(l2!=NULL)
+    {
+        while(n1 and n1->data<=l2->data)
+        {
+            n1=n1->next;
+        }
+        while(l1->next!=n1)
+        {
+            l1=l1->next;
+        }
+        l1->next=l2;
+        l1=l2;
+        l2=n1;
+        n1=l1;
+    }
+    return res;
+}
